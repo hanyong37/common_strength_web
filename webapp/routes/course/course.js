ad06a960b2beef13665faefcb046fd4799844364
ziming.gong <gzm@iyooh.net>
2016-09-09 01:29:49 +0800

    课程管理列表和新增页面

--- webapp/routes/course/course.js
+++ webapp/routes/course/course.js
@@ -4,4 +4,19 @@ module.exports = function(app){
     res.render('course_rules.html');
   });
 
+  app.get("/courseManagement", function(req, res) {
+    res.render('course_management.html');
+  });
+
+  app.get("/courseManagementAdd", function(req, res) {
+    res.render('course_management_add.html');
+  });
+
+  app.get("/courseSort", function(req, res) {
+    res.render('course_sort.html');
+  });
+
+  app.get("/courseSortAdd", function(req, res) {
+    res.render('course_sort_add.html');
+  });
 };
