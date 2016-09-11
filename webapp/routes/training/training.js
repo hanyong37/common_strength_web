56322854d2df83e2433c4cec2be1bb0381909a43
guanjp <nineguan@foxmail.com>
2016-09-11 15:37:53 +0800

    修改

--- webapp/routes/training/training.js
+++ webapp/routes/training/training.js
@@ -2,4 +2,10 @@ module.exports = function(app){
   app.get('/trainingHistory', function(req, res){
     res.render('training_history.html');
   });
+  app.get('/trainingReservation', function(req, res){
+    res.render('training_reservation.html');
+  });
+  app.get('/trainingTourist', function(req, res){
+    res.render('training_tourist.html');
+  });
 };
