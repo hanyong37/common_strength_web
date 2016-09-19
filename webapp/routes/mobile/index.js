0b7d1ae02f16f7ef40b188fc033f970cb2e06681
ziming.gong <gzm@iyooh.net>
2016-09-19 11:58:34 +0800

    修改

--- webapp/routes/mobile/index.js
+++ webapp/routes/mobile/index.js
@@ -5,5 +5,9 @@ module.exports = function(app) {
 
   app.get('/app/messageCenter', function(req, res) {
     res.render('mobile/message_center.html');
-  })
+  });
+
+  app.get('/app/my', function(req, res) {
+    res.render('mobile/my.html');
+  });
 }
