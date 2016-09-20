19c817a36659631c9b6718db3a46b204db2ab057
guanjp <nineguan@foxmail.com>
2016-09-21 00:18:40 +0800

    修改

--- webapp/es6/store_manage.js
+++ webapp/es6/store_manage.js
@@ -1,10 +1,26 @@
 $(function() {
   const Main = {
     init: () => {
-
+      Main.getStoreList();
     },
     getStoreList: () => {
-
+      $.ajax({
+        url: '/api/member/getMemberShipsInfo',
+        type: 'get',
+        dataType: 'json',
+        data: {
+          currentPage: 1,
+          pageSize: 10,
+          keyword: ''
+        },
+        success: function(result){
+          console.log(result);
+        }
+      });
     },
   };
+
+  //http://123.207.151.199:8080/member/getMemberShipsInfo
+
+  Main.init();
 });
