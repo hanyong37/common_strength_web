61e1623457c28695863040341a0309710ad61e54
ziming.gong <gzm@iyooh.net>
2016-09-22 10:12:59 +0800

    接口调试

--- webapp/es6/store_manage_add.js
+++ webapp/es6/store_manage_add.js
@@ -1,15 +1,46 @@
 
 const Main = {
-  init: () => {
+  init: ()=> {
     $('.selectpicker').selectpicker({
       size: 4
     });
+    $('#j-save').on('click', function() {
+      Main.saveStoreInfo();
+    });
   },
-  saveStoreInfo: () => {
-    var uname = $.trim($('.input-username').val());
-    var pwd = $.trim($('.input-password').val());
+  saveStoreInfo: ()=> {
+      // var uname = $.trim($('.input-username').val());
+      // var pwd = $.trim($('.input-password').val());
+      var _storeName = $.trim($('#j-name').val());
+      var _storeAddress = $.trim($("#j-address").val());
+      var _storeDescription = $.trim($("#j-text").val());
 
-  },
+      Main.__ajax({
+        storeId: '',
+        storeName: _storeName,
+        storeAddress: _storeAddress,
+        storeDescription: _storeDescription
+      });
+   },
+  __ajax: (a)=> {
+    $.ajax({
+      url: '/api/store/updateStoreInfo',
+      data: {
+        storeId: a.storeId,
+        storeName: a.storeName,
+        storeAddress: a.storeAddress,
+        storeDescription: a.storeDescription
+      },
+      type: 'post',
+      dataType: 'json',
+      success: function(result) {
+        console.log(result);
+        if (result.code == 1) {
+
+        }
+      }
+    })
+  }
 };
 
 (function(){
