f896d40af878460000376b6d9fde7d38331572d1
ziming.gong <zuigzm@gmail.com>
2016-11-07 22:20:58 +0800

    修改

--- webapp/es6/mobile/my.js
+++ webapp/es6/mobile/my.js
@@ -1,18 +1,29 @@
 const Main = {
   init: () => {
-
+    Main.getMyInfo();
   },
   getMyInfo: () => {
     $.ajax({
       url: '/api/weixin/my_info',
-      data: {
-
+      headers: {
+        'X-Api-Key': Wx.token,
       },
       type:'get',
       dataType: 'json',
-      success: () => {
-
+      success: (result) => {
+        if (result.data) {
+          const attributes = result.data.attributes;
+          console.log(attributes);
+          CS.setNunjucksTmp({
+            tmpSelector: '#tmp',
+            boxSelector: '.cs-body',
+            isAppend: 'append',
+            data: attributes
+          });
+        }
       }
     })
   }
-}
+};
+
+Main.init();
