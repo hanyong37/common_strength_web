312b24f5f29d345d40495d1335696e78e8d9db5b
guanjp <nineguan@foxmail.com>
2016-11-11 13:40:15 +0800

    启动修改，bug修改

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export LOG=/Users/junpeng.guan/myspace/iyooh-management/webapp/logs
-export APP_PATH=/Users/junpeng.guan/myspace/iyooh-management/webapp
+export LOG=/var/www/common_strength/web/iyooh-management/webapp/logs
+export APP_PATH=/var/www/common_strength/web/iyooh-management/webapp
 export APP=$APP_PATH/app.js
 
 forever -p $APP_PATH -l $LOG/access.log -e $LOG/error.log -o $LOG/out.log  --watchDirectory $APP_PATH  -aw start $APP
